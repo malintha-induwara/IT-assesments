1fd06b74bb8613c0ef6d2918c91133e3924c917d
Induwara <induwara2k@gmail.com>
2024-03-30 15:28:33 +0530

    primitive data types js added

---
+++ Assesment 1/js/lesson-js-primitive-datea-types.js
@@ -0,0 +1,27 @@
+//Sring
+let firstName = "Hello";
+console.log("Type of firsName:", typeof firstName);
+
+//Number
+let age = 30;
+console.log("Type of age:", typeof age);
+
+//Float
+let distance = 3.24;
+console.log("Type of distance:", typeof distance);
+
+//Boolean
+let isDeleted = false;
+console.log("Type of isDeleted:", typeof isDeleted);
+
+//Undefined
+let address;
+console.log("Type of address", typeof address);
+
+let email = undefined;
+console.log("Type of email:", typeof email);
+
+let phoneNumber = null;
+console.log("Type of phoneNumber: ", typeof phoneNumber);
+
+// Symbol - Intrroduced in ECMAScript  6 (ES6), symbols are unique and imutable primitive values
