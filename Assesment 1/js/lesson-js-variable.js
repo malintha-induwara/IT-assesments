1971c7818f05048832bb756ef601b7c7cb18f21e
Induwara <induwara2k@gmail.com>
2024-03-30 14:48:09 +0530

    add js files

---
+++ Assesment 1/js/lesson-js-variable.js
@@ -0,0 +1,16 @@
+// To Define variables in js there are three keywords
+// var,let,const
+//
+
+let firstName = "Hello";
+let lastName = "Word";
+
+var age = 30;
+const salary = 5000.0;
+salary = 400;
+
+// Imporrant
+
+//1. cannot be a reserved word
+//2. variable name should be meaningfull
+//3. Cannot start with numbers
---
+++ Assesment 1/js/lesson-js.js
@@ -0,0 +1,8 @@
+console.log("Hello universe");
+
+console.log(10 + 20);
+
+console.log("Add:" , (10 + 20));
+console.log("Sub:" , (10 - 20));
+console.log("Devide:" , 20 / 10);
+console.log("Multi:" , 10 * 20);
