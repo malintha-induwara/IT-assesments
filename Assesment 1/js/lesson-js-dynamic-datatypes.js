6f83e1abdbdc8f5257eec0d9eee3a028ce4ba567
Induwara <induwara2k@gmail.com>
2024-03-30 15:38:42 +0530

    lesson dynamic data types added

---
+++ Assesment 1/js/lesson-js-dynamic-datatypes.js
@@ -0,0 +1,15 @@
+let testingVariale;
+
+console.log("Type of Testing Variable", typeof testingVariale);
+
+testingVariale = "Hello";
+console.log("Type of Testing Variable", typeof testingVariale);
+
+testingVariale = 20;
+console.log("Type of Testing Variable", typeof testingVariale);
+
+testingVariale = 23.499;
+console.log("Type of Testing Variable", typeof testingVariale);
+
+testingVariale = false;
+console.log("Type of Testing Variable", typeof testingVariale);
