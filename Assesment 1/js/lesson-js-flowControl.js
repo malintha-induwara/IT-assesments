2ef0744402831c8de6f549621df9e7fce613fd85
Induwara <induwara2k@gmail.com>
2024-04-06 12:18:36 +0530

    flow control update

--- Assesment 1/js/lesson-js-flowControl.js
+++ Assesment 1/js/lesson-js-flowControl.js
@@ -34,3 +34,43 @@ switch (dayOfWeek) {
     console.log("Invalid date");
     break;
 }
+
+//===========Loops=========
+
+//1)For Loop
+
+for (let i = 0; i < 5; i++) {}
+
+//2)While loop
+
+let j = 5;
+while (j < 5) {
+  j++;
+}
+
+//3) Do while
+
+let k = 6;
+do {
+  k++;
+} while (k < 6);
+
+//=========4 Break========
+
+for (let n = 0; n < 5; n++) {
+  if (n == 4) {
+    break;
+  }
+  console.log(n);
+}
+
+console.log();
+
+//=========5 Continute========
+
+for (let n = 0; n < 5; n++) {
+  if (n == 4) {
+    continue;
+  }
+  console.log(n);
+}
