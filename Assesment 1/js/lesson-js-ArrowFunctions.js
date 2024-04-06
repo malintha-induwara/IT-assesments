c51bba6692901b636ee024dec772b7e44f4baea6
Induwara <induwara2k@gmail.com>
2024-04-06 13:02:55 +0530

    arrow function update

---
+++ Assesment 1/js/lesson-js-ArrowFunctions.js
@@ -0,0 +1,14 @@
+
+function addNumbers(num1,number2) {
+    return num1+number2;
+}
+
+
+//Arrow Functions
+const addNummbersArrowFunc = (num1, num2) =>{
+    return num1+num2;
+}
+
+
+console.log( addNumbers(5,4));
+console.log( addNummbersArrowFunc(5,4))
