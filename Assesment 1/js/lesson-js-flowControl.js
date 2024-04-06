57b1887e339be6c6078a2d9ba8d52f2f8cee1fcc
Induwara <induwara2k@gmail.com>
2024-04-06 12:03:41 +0530

    content update

--- Assesment 1/js/lesson-js-flowControl.js
+++ Assesment 1/js/lesson-js-flowControl.js
@@ -1,10 +1,36 @@
+//If statement
+
 let number = 0;
 
 if (number > 0) {
-    console.log("Number is positive");
-}else if (number < 0) {
-    console.log("Number is negative");
-}else {
-    console.log("Number is zero");
+  console.log("Number is positive");
+} else if (number < 0) {
+  console.log("Number is negative");
+} else {
+  console.log("Number is zero");
 }
-    
+
+//Switch statement
+
+let dayOfWeek = 1;
+
+switch (dayOfWeek) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+  case 3:
+    console.log("Tuesday");
+  case 4:
+    console.log("Wednesday");
+  case 5:
+    console.log("Thursday");
+  case 6:
+    console.log("Friday");
+  case 7:
+    console.log("Saturday");
+  default:
+    console.log("Invalid date");
+    break;
+}
