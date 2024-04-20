a59b8dd44c218ce67d8293230a09f31ff792e058
Induwara <induwara2k@gmail.com>
2024-04-20 12:17:22 +0530

    arrow function update

--- Assesment 1/js/lesson-js-ArrowFunctions.js
+++ Assesment 1/js/lesson-js-ArrowFunctions.js
@@ -12,3 +12,17 @@ const addNummbersArrowFunc = (num1, num2) =>{
 
 console.log( addNumbers(5,4));
 console.log( addNummbersArrowFunc(5,4))
+
+
+//Converting annonymous function to arrow function
+let multiplyNumbers = (num1, num2) => num1 * num2;
+
+
+
+//Converting constructor  function to arrow function
+//Syntax wise this is possible
+//but dont do this because "this" and "new" keyword will not work in arrow function
+let Student = (name, age) =>{
+    this.name = name;
+    this.age = age;
+}
