20c7cf479943744522679ef2eb1b9c420fc420ec
Induwara <induwara2k@gmail.com>
2024-04-20 15:58:50 +0530

    js element selection

--- Assesment 1/js/lesson-js-element-selection.js
+++ Assesment 1/js/lesson-js-element-selection.js
@@ -1,1 +1,10 @@
 console.log("Hey there")
+
+
+//ID selection
+let calculateButton = document.getElementById('btnCalculate')
+console.log(calculateButton)
+
+//Class selection
+let inputField = document.getElementsByClassName('box')
+console.log(inputField)
