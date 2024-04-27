2b1a19f60253202cee7db10f35e8bf9775417c2c
Induwara <induwara2k@gmail.com>
2024-04-27 16:24:23 +0530

    event listner update

--- Assesment 1/js/lesson-js-EventListners.js
+++ Assesment 1/js/lesson-js-EventListners.js
@@ -5,12 +5,28 @@ box.addEventListener("click", () => {
   alert("how are you");
 });
 
+//Mouse over
+
 box.addEventListener("mouseover", () => {
   console.log("Yooo");
   box.style.backgroundColor = "red";
 });
 
+//Mouse out
+
 box.addEventListener("mouseout", () => {
   box.style.backgroundColor = "green";
   console.log("Nooo");
+});
+
+//Kerdown event
+
+let userNameField = document.getElementById("userName");
+
+userNameField.addEventListener("keydown", () => {
+  console.log("keydown");
+});
+
+userNameField.addEventListener("keyup", () => {
+  console.log("keyup");
 });
