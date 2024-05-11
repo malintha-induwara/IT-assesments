eda32b844bcce7f0090c10af36429361267ec5a4
Induwara <induwara2k@gmail.com>
2024-05-11 09:05:50 +0530

    exesise16 complete

--- Assesment 1/js/exersise16.js
+++ Assesment 1/js/exersise16.js
@@ -3,5 +3,6 @@ const numbersId = document.getElementById("numbers");
 numbersId.addEventListener("keypress", (event) => {
   if (event.key < "0" || event.key > "9") {
     event.preventDefault();
-  }
+     alert("Key is pressed");
+  } 
 });
