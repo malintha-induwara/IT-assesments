3e3687d811fc27be74c657be95b6fdd36a078509
Induwara <induwara2k@gmail.com>
2024-05-11 10:14:04 +0530

    add lesson Jquery

--- Assesment 1/js/exersise16.js
+++ Assesment 1/js/exersise16.js
@@ -2,7 +2,8 @@ const numbersId = document.getElementById("numbers");
 
 numbersId.addEventListener("keypress", (event) => {
   if (event.key < "0" || event.key > "9") {
+    console.log(event);
     event.preventDefault();
-     alert("Key is pressed");
-  } 
+    alert("Key is pressed");
+  }
 });
